aa0b6d5fd35d279485468833109209c3c7b344cd
Abhiiiiijit <abhijeetbosedas560@gmail.com>
2023-10-09 00:05:29 +0530

    API Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
-
+import "./App.css";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./apis/api";
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
 
   return (
     <>
-    <div className="App">App</div>
+      <div className="App">App</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/apis/api.js
+++ src/apis/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+// OR
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNGQxMjVjODUxMzM5ODI0ODNjZmFiMjNkMTIzYTdkNSIsInN1YiI6IjY1MGFkNjI5ODFjN2JlMDEzYWYxNzEwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EEE-utAFP4U_5_TKaWaCGOp14k3xUy0suSiyYTIqRXIz";
 
 // Check on tmdb Website
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
-
+//tmdb API Calling to get the data of Movies and TV Shows
 export const fetchDataFromApi = async (url, params) => {
   try {
     // Getting Result and Configuration like headers and parameter
